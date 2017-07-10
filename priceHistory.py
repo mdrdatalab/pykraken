344459879825040cc6b125b7ec9b5565d18686a5
Michael Dustin Riley Riley <mdrdatalab@gmail.com>
2017-07-10 16:28:08 -0400

    compute ohlc
    given a window of trades, computes open, close, low, high, volume
    weighted average, volume, and count over that period

--- priceHistory.py
+++ priceHistory.py
@@ -8,18 +8,28 @@ Created on Sun Jul  2 20:05:15 2017
 import queries
 import time
 import csv
-
+import os.path
+import datetime
+import numpy as np
 
 def get_history(asset):
-    #TODO: first check if price history has been saved, load it if so
+ 
     pairs = queries.get_pairs()
     pairs = list(pairs.keys())
-    #I don't like only having it in USD anymore... fix this?
-    usd = asset+'ZUSD'
+    last = 0    
     trades = []
+    #TODO: I don't like only having it in USD anymore... fix this?
+    usd = asset+'ZUSD'
+    
+    #TODO: first check if price history has been saved, load it if so       
+    if os.path.isfile('data\\'+usd+'-history.csv'):
+        history = hist_reader(usd)
+        last = history['last']
+        trades = history['trades']
+    #why list doesn't come out flat?
     if usd in pairs:
         print('yes')
-        last = 0
+
         temp = {usd: ['x'], last: 0}
         while temp[usd] != []:
             try:
@@ -30,13 +40,10 @@ def get_history(asset):
                 time.sleep(2)
             except Exception as e:
                 pass
-        
-    return {'trades': trades, 'last': last}
-
-
-a = time.time()
-history = get_history('XETH')
-print(time.time()- a)        
+    history = {'trades': trades, 'last': last}
+    hist_writer(usd, history)    
+    return history
+    
 
 def hist_writer(pair, history):  
     with open('data\%s-history.csv' % pair, 'w', newline='') as f:
@@ -54,4 +61,38 @@ def hist_reader(pair):
             trades.append(row)
     history = {'last': last, 'trades': trades}
     return history
+
+
+
+def compute_ohlc(trades):
+    '''
+    takes a window of trades and computes open price, close price, low price,
+    high price, volume weighted average price, volume, and count
+    '''
+    trades = np.array(trades)[:,0:3].astype(float)
+    count = len(trades)
+    p_open = trades[0][0]
+    p_close = trades[-1][0]    
+    p_high = max(trades[:,0])
+    p_low = min(trades[:,0])
+    volume = sum(trades[:,1])
+    vwap = sum(trades[:,0]*trades[:,1])/volume
+    ohlc = {'open': p_open,
+            'close': p_close,
+            'high': p_high,
+            'low': p_low,
+            'volume': volume,
+            'vwap': vwap,
+            'count': count}
+            
+    return ohlc
+   
+    
+def get_ohlc(pair, interval=1440, start=None, end=None):
+    '''
+    retrieves trade history for a pair between start and end
+    and returns ohlc data for that window at specified interval
+    '''
+    pass
+    
     
